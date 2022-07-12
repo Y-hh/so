f955f4600f8cfb2426359a29c0cd14694a1ab74f
y-hh <yanjunzhe-y@qq.com>
2022-07-12 16:40:17 +0800

    files

---
+++ javaSE/src/Demo01.java
@@ -0,0 +1,10 @@
+//kinf
+public class Demo01 {
+    //
+    String name;
+    int age;
+    public void study(){
+      System.out.println("hello woral");
+      System.out.println(this.name+"is study");
+    }
+}
---
+++ javaSE/src/Demo02.java
@@ -0,0 +1,7 @@
+
+public class Demo02{
+  public static void main(String[] args) {
+    System.out.print("in");
+  }  
+   
+}
