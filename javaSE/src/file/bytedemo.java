a29440a5ad206ed05397d139366c9835f29c95a5
y-hh <yanjunzhe-y@qq.com>
2022-08-18 18:33:37 +0800

    删除了out目录

---
+++ javaSE/src/file/bytedemo.java
@@ -0,0 +1,10 @@
+package file;
+/**
+ * 输出流：OutputStream 对文件的输出流用子类FileOutputStream
+ * 输入流：InputStream 对文件的输入流用子类FileInputStream
+ */
+public class bytedemo {
+    public static void main(String[] args) {
+        
+    }
+} 
